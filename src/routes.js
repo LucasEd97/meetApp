b6d9ee2fb62b2bac23eaa985c5613f6ae6e6b364
Lucas Ferreira <lucas.98713@gmail.com>
2019-08-05 22:13:11 -0300

    Cria Migration Users

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.post('/users', UserController.store);
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Lucas Eduardo',
+    email: 'lucas@email.com',
+    password_hash: '12345678',
+  });
+
+  return res.json(user);
+});
+
+module.exports = routes;
